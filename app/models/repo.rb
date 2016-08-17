ffc9276b2da3dfc2c5a55f9c7fda484cf9c6555e
Matthew Zikherman <matt@bestvendor.com>
2016-08-17 15:45:56 -0400

    match prefixes

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,5 +1,8 @@
 class Repo < ActiveRecord::Base
   include PgSearch
   pg_search_scope :search_readme,
-                  against: [:name, :description, :readme]
+                  against: [:name, :description, :readme],
+                  using: {
+                  	tsearch: { prefix: true }
+                  }
 end
