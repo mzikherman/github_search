d6e54d78fb051424ff196a867a969aa6d07b8090
Matthew Zikherman <matt@bestvendor.com>
2016-08-17 17:11:49 -0400

    better searchg

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,9 +16,14 @@ module ApplicationHelper
 
     renderer = Redcarpet::Render::HTML.new(options)
     markdown = Redcarpet::Markdown.new(renderer, extensions)
+    # Do the image thing
+
     text = text.gsub('public/images/shared', "https://github.com/artsy/#{name}/raw/master/public/images/shared")
     text = text.gsub('app/assets/images/embed', "https://github.com/artsy/#{name}/raw/master/app/assets/images/embed")
+    text = text.gsub('doc/images', "https://github.com/artsy/#{name}/raw/master/doc/images")
+    # text = text.gsub('images/', "https://github.com/artsy/#{name}/raw/master/images/")
     
+    # Remove this weird markdown
     text = text.gsub(' &quot;Artsy&quot;', '')
     markdown.render(text).html_safe
   end
